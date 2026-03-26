dd92001fd010af4942f95c3225683910f3694041
braydenpb72 <braydenpb72@gmail.com>
2026-03-25 20:58:55 -0700

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   clearScreen: false,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 1420,
     strictPort: true,
